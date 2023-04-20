6ae6347615165204a83e0116a1f75f92541c03c6
Marcos Anton <antonbarroso16@gmail.com>
2023-04-20 17:34:58 -0300

    Initial commit

---
+++ app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import 'dotenv/config'
+
+
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+
+app.get('',(req,res)=>{
+    res.send('hola pibe')
+})
+
+app.listen(port, ()=>{
+    console.log(`El servidor esta iniciado en ${port}`);
+})
